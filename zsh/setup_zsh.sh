c3a8bc874847f803b7a92d1c7d8281d60a087d52
neotod <neotod@protonmail.com>
2022-09-08 14:41:28 +0430

    updating setup_zsh: fixing prints + removing config files before making symlinks

--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -1,36 +1,36 @@
 BASE_DIR=$1 # BASE_DIR passed from Makefile
 BASE_DIR=${BASE_DIR:0:-1}
 
-echo "\n--- installing required pkgs ---\n"
+printf "\n--- installing required pkgs ---\n"
 
-echo "\ninstalling wget..."
+printf "\ninstalling wget..."
 sudo pacman -S wget
 
-echo "\ninstalling zsh..."
+printf "\ninstalling zsh..."
 sudo pacman -S zsh
 
-echo "\ninstalling and configuring OMZ..."
+printf "\ninstalling and configuring OMZ..."
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "\ninstalling and configuring p10k"
+printf "\ninstalling and configuring p10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-echo "\ninstalling vivid for LS_COLORS theme..."
+printf "\ninstalling vivid for LS_COLORS theme..."
 sudo pacman -S vivid
 
-echo "\n--- installing required pkgs -> finished ---\n"
+printf "\n\n--- installing required pkgs -> finished ---\n"
 
-echo "\n--- installing zsh plugins ---\n"
+printf "\n--- installing zsh plugins ---\n"
 
-echo "\ninstalling zsh-autosuggestions"
+printf "\ninstalling zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-echo "\ninstalling zsh-syntax-highlighting"
+printf "\ninstalling zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-echo "\n--- installing zsh plugins -> finished ---\n"
+printf "\n\n--- installing zsh plugins -> finished ---"
 
-echo "making zsh configs symlinks..."
+printf "\n\n--- making zsh configs symlinks ---"
 
 home_target_configs=(
   $BASE_DIR/zsh/.zshrc
@@ -44,7 +44,8 @@ home_configs_paths=(
   $HOME/.p10k.zsh
 )
 
-echo "\nmaking configs symlinks..."
+printf "\n\nmaking configs symlinks..."
 for i in ${!home_target_configs[@]}; do
+  rm "${home_configs_paths[i]}"
   ln -sv "${home_target_configs[i]}" "${home_configs_paths[i]}"
 done
