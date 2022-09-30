36b58f76f0546fb93424a39332ce853867e03847
neotod <neotod@protonmail.com>
2022-09-30 13:42:40 +0330

    upadting setup_zsh so it can download OMZ w/ curl instead of wget

--- scripts/setup_proxy.sh
+++ scripts/setup_proxy.sh
@@ -4,4 +4,4 @@ sudo pacman -S --needed yay
 yay -S --needed clash-for-windows-bin
 
 echo -e "\nClash has been installed successfuly"
-echo -e "\ngo to this link for latest proxies: https://docs.google.com/document/d/1KPaCBfGIkFhD51i6ArrTgbsDKHtgQZALndtPrr4KnEw"
+echo -e "\ngo to this link for latest proxies: https://docs.google.com/document/d/1KPaCBfGIkFhD51i6ArrTgbsDKHtgQZALndtPrr4KnEw "
--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -3,6 +3,11 @@ BASE_DIR=${BASE_DIR:0:-1}
 
 echo -e "\n--- installing required pkgs ---\n"
 
+echo -e "\n Note: If internet is fucked up and you require to set a proxy server, set it up fist :). \n"
+echo -e "example: export HTTP_PROXY=socks5://0.0.0.0:9999 &&  export HTTPS_PROXY=socks5://0.0.0.0:9999  &&  export SOCKS_PROXY=socks5://0.0.0.0:9999 \n"
+
+sleep 1
+
 echo -e "\ninstalling wget..."
 sudo pacman -S wget
 
@@ -10,7 +15,7 @@ echo -e "\ninstalling zsh..."
 sudo pacman -S zsh
 
 echo -e "\ninstalling and configuring OMZ..."
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo -e "\ninstalling and configuring p10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
